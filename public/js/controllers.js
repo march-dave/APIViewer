b261ae42df6dccf4afd0446e5fdae3c7b8100600
march-dave <ysangy@gmail.com>
2016-04-30 23:02:40 -0700

    MVP

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,48 +1,142 @@
 'use strict';
 
-var app = angular.module('routerApp');
+var app = angular.module('starWarsApp');
 
-app.controller('listCtrl', function($scope, $state, People) {
-  People.getAll()
-  .then(people => {
-    $scope.people = people;
-  });
+
+app.controller('homeCtrl', function($q, $http) {
+  console.log('homeCtrl111111');
 });
 
-app.controller('detailCtrl', function($scope, person) {
-  $scope.person = person;
+app.controller('listCtrl', function($scope, $state, starDex) {
+  console.log('listCtrl1111', starDex);
+  $scope.starDex = starDex;
 });
 
-app.controller('homeCtrl', function() {
-  console.log('homeCtrl!');
-});
+app.controller('detailCtrl', function($scope, $interval, starwars) {
+  $scope.starwars = starwars;
 
-app.controller('contactCtrl', function($scope, $state, SweetAlert) {
-  console.log('contactCtrl!');
+  console.log('starwars', starwars);
 
-  $scope.showAlert = () => {
-    SweetAlert.swal({
-      title: "Are you sure?",
-      text: "You want to go home?",
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#DD6B55",
-      confirmButtonText: "Yes, go home!",
-      closeOnConfirm: true
-    }, function(isConfirm) {
-      if(isConfirm) {
-        $state.go('home');
-      }
-    });
-  };
-});
+  // var sprites = starwars.sprites;
+  // var keys = Object.keys(sprits);
+  // var numSprites = key.length;
 
-app.controller('starwarslistCtrl', function($scope, PeopleList) {
-  console.log('starWarsListCtrl!');
+  // Just for comment out Start Wars images data key value name verify
+  // var count = 0;
+  // $interval(function() {
+  //   count++;
+  //
+  //   var index = count % numSprites;
+  //   var spriteUrl = sprites[keys[index]];
+  //   $scope.sprite = spriteUrl;
+  //
+  // }, 1000);
 
-  PeopleList.getAll()
-  .then(peopleList => {
-    $scope.peopleList = peopleList;
-  });
+})
 
-});
+// app.controller('homeCtrl', function($scope, $state, PeopleList) {
+//
+//   $scope.getCharacter = function (index) {
+//     $state.go('about', {
+//       peopleIndex: index
+//     })
+//   }
+//   PeopleList.getAll()
+//    .then(peopleList => {
+//      $scope.peopleList = peopleList;
+//    });
+// });
+//
+// app.controller('aboutCtrl', function ($stateParams, $scope, person) {
+//   // $scope.thisCharacter = SwapiService.characters[$stateParams.peopleIndex];
+//
+//   // {{person.name}} - {{person.height}} - {{person.hair_color}}
+//
+//   // var person = {
+//   //   name: "Luke Skywalker",
+//   //   height: "172",
+//   //   hair_color: "red"
+//   // }
+//
+//   // console.log('person', person);
+//
+//   //$scope.thisCharacter = person;
+//
+//   // console.log('$stateParams', $stateParams);
+//   // console.log('$scope', $scope);
+//   // console.log('person', person);
+// });
+//
+//
+//
+// //   $scope.getCharacter = function(index) {
+// //     // $state.go('starwarsdetails', {
+// //
+// //     $state.go('detail', {
+// //       peopleIndex: index
+// //     })
+// //   }
+// //
+// //   PeopleList.getAll()
+// //   .then(peopleList => {
+// //     $scope.peopleList = peopleList;
+// //   });
+// // });
+//
+// // app.controller('detailCtrl', function($scope, person) {
+// //   $scope.person = person;
+// // });
+// app.controller('detailCtrl', function($scope, person) {
+//   // $scope.person = person;
+//   console.log('dsfksdhsfdjhkks');
+// });
+//
+// // app.controller('starwarsdetailsCtrl', function ($stateParams, $scope) {
+// //   console.log('dsfksdhsfdjhkks');
+// // });
+//
+//
+//
+// app.controller('listCtrl', function($scope, $state, People) {
+//   People.getAll()
+//   .then(people => {
+//     $scope.people = people;
+//   });
+// });
+//
+//
+// app.controller('starwarslistCtrl', function($scope, $state, PeopleList) {
+//   $scope.getCharacter = function(index) {
+//     $state.go('starwarsdetails', {
+//       peopleIndex: index
+//     })
+//   }
+//
+//   PeopleList.getAll()
+//   .then(peopleList => {
+//     $scope.peopleList = peopleList;
+//   });
+// });
+//
+// app.controller('starwarsdetailsCtrl', function ($stateParams, $scope) {
+//   console.log('dsfksdhsfdjhkks');
+// });
+//
+// app.controller('contactCtrl', function($scope, $state, SweetAlert) {
+//
+//   $scope.showAlert = () => {
+//     SweetAlert.swal({
+//       title: "Are you sure?",
+//       text: "You want to go home?",
+//       type: "warning",
+//       showCancelButton: true,
+//       confirmButtonColor: "#DD6B55",
+//       confirmButtonText: "Yes, go home!",
+//       closeOnConfirm: true
+//     }, function(isConfirm) {
+//       if(isConfirm) {
+//         $state.go('home');
+//       }
+//     });
+//   };
+// });
